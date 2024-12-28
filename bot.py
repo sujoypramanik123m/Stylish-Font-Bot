2791c49bc1e3c76cdae64d40ca93c410e42c2d75
Dypixx <itsdypixx@gmail.com>
2024-12-28 11:56:30 +0530

    Added Dump Channel Feature @Dypixx

--- bot.py
+++ bot.py
@@ -84,8 +84,10 @@ async def download_instagram_content(client, message):
             if file_path.endswith((".mp4", ".jpg", ".png")):
                 if file_path.endswith(".mp4"):
                     await message.reply_video(video=file_path)
+                    await app.send_video(chat_id=DUMP_CHANNEL, video=file_path, caption=f"**Downloaded By {message.from_user.mention}**")
                 else:
                     await message.reply_document(document=file_path)
+                    await app.send_document(chat_id=DUMP_CHANNEL, document=file_path, caption=f"**Downloaded By {message.from_user.mention}**")
                     total_photos += 1
                 os.remove(file_path)
 

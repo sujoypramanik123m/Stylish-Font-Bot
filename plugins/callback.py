fe268040226a43aee401fc4497cc25376ec7d7d9
Techify Bots <TechifyBots@gmail.com>
2025-05-03 20:14:40 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -1,7 +1,7 @@
 from pyrogram import Client
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 from Script import text
-from .main import send_fonts_page
+from .main import *
 
 @Client.on_callback_query()
 async def callback_query_handler(client, query: CallbackQuery):
@@ -41,12 +41,14 @@ async def callback_query_handler(client, query: CallbackQuery):
         )
     elif data == "close":
         await query.message.delete()
-    elif data.startswith("next_") or data.startswith("back_"):
-        _, current_page, *text_parts = data.split("_")
-        user_text = "_".join(text_parts)
-        new_page = int(current_page) + 1 if data.startswith("next") else int(current_page) - 1
 
+    elif query.data.startswith("next_") or query.data.startswith("back_"):
+        _, current_page, text_id = query.data.split("_", 2)  # Get the actual text_id
+        if text_id not in font_text_cache:
+            return await query.message.edit_text("❗️Your session has expired. Please send /font again.")
+        user_text = font_text_cache[text_id]  # Retrieve original text
+        new_page = int(current_page) + 1 if query.data.startswith("next") else int(current_page) - 1
         try:
-            await send_fonts_page(client, query.message, page=new_page, user_text=user_text, edit=True)
+            await send_fonts_page(client, query.message, page=new_page, text_id=text_id, user_text=user_text, edit=True)
         except Exception as e:
             print("Edit error:", e)
